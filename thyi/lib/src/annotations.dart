bd065bb4fd720f0e9a6320384f6ec41d7a94ea59
th <huhuang03@gmail.com>
2019-11-26 13:45:05 +0800

    add Annotations

---
+++ thyi/lib/src/annotations.dart
@@ -0,0 +1,26 @@
+class HEADER {
+  final String key;
+  const HEADER(this.key);
+}
+
+class POST {
+  final String path;
+  const POST(this.path);
+}
+
+
+class GET {
+  final String path;
+  const GET(this.path);
+}
+
+
+class FIELD {
+  final String key;
+  const FIELD(this.key);
+}
+
+class QUERY {
+  final String key;
+  const QUERY(this.key);
+}
